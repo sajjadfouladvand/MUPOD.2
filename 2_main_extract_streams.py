455312646d75f926c4d1c252c456a561935c3a02
Sajjad Fouladvand <34069771+sajjadfouladvand@users.noreply.github.com>
2021-07-23 18:53:21 -0400

    Adding log

--- 2_main_extract_streams.py
+++ 2_main_extract_streams.py
@@ -3,33 +3,43 @@ import argparse
 import sys
 import os
 import utils.extract_streams as ext_strs
-
+import logging
 
 sys.path.append(os.getcwd())
 parser = argparse.ArgumentParser()
 parser.add_argument("--cohort", type=str, default='oud_yes', choices = ['oud_yes','oud_no'])    
-parser.add_argument("--display_step", type=int, default=100000)    
+parser.add_argument("--display_step", type=int, default=1000000)    
+parser.add_argument("--logging_milestone", type=int, default=10)    
 
 if  parser.parse_args().cohort == "oud_yes":
-    parser.add_argument("--meds_rawdata_filename", type=str, default='data/sampled_oud_yes_icd_presc_based_prescriptions_view_sorted.csv')    
-    parser.add_argument("--diags_rawdata_filename", type=str, default='data/sampled_oud_yes_icd_presc_based_diagnoses_view_sorted.csv')
-    parser.add_argument("--procs_rawdata_filename", type=str, default='data/sampled_oud_yes_icd_presc_based_procedures_view_sorted.csv')    
+    parser.add_argument("--meds_rawdata_filename", type=str, default='data/summarized_oud_yes_icd_presc_based_prescriptions_view_sorted.csv')    
+    parser.add_argument("--diags_rawdata_filename", type=str, default='data/summarized_oud_yes_icd_presc_based_diagnoses_view_sorted.csv')
+    parser.add_argument("--procs_rawdata_filename", type=str, default='data/summarized_oud_yes_icd_presc_based_procedures_view_sorted.csv')    
 elif parser.parse_args().cohort == "oud_no":    
-    parser.add_argument("--meds_rawdata_filename", type=str, default='data/sampled_oud_no_icd_presc_based_prescriptions_view_sorted.csv')    
-    parser.add_argument("--diags_rawdata_filename", type=str, default='data/sampled_oud_no_icd_presc_based_diagnoses_view_sorted.csv')
-    parser.add_argument("--procs_rawdata_filename", type=str, default='data/sampled_oud_no_icd_presc_based_procedures_view_sorted.csv')    
+    parser.add_argument("--meds_rawdata_filename", type=str, default='data/summarized_oud_no_icd_presc_based_prescriptions_view_sorted.csv')    
+    parser.add_argument("--diags_rawdata_filename", type=str, default='data/summarized_oud_no_icd_presc_based_diagnoses_view_sorted.csv')
+    parser.add_argument("--procs_rawdata_filename", type=str, default='data/summarized_oud_no_icd_presc_based_procedures_view_sorted.csv')    
 
 args = parser.parse_args()
 
+logging.basicConfig(format='Date-Time : %(asctime)s : Line No. : %(lineno)d - %(message)s', level = logging.INFO, filename = 'log/logfile_extract_streams.log', filemode = 'a')
+
+
+
 ext_strs.extract_medications(args.meds_rawdata_filename 
                                     , args.cohort
-                                    , args.display_step)
+                                    , args.display_step
+                                    , args.logging_milestone)
 
 ext_strs.extract_diagnoses(args.diags_rawdata_filename 
                                     , args.cohort
-                                    , args.display_step)
+                                    , args.display_step
+                                    , args.logging_milestone)
 
 ext_strs.extract_procedures(args.procs_rawdata_filename 
                                     , args.cohort
-                                    , args.display_step)
+                                    , args.display_step
+                                    , args.logging_milestone)
 
+logging.info('Extracting medications, diagnoses and procedures streams successfully completed!')
+logging.info('================================')
--- utils/extract_streams.py
+++ utils/extract_streams.py
@@ -5,6 +5,9 @@ from dateutil.relativedelta import *
 import pdb
 import os
 import time
+import logging
+
+logging.basicConfig(format='Date-Time : %(asctime)s : Line No. : %(lineno)d - %(message)s', level = logging.INFO, filename = 'log/logfile_extract_streams.log', filemode = 'a')
 
 
 def diff_month(d1, d2):
@@ -31,7 +34,8 @@ def vdates_syncher(current_patient_vdates):
 
 def extract_medications(meds_rawdata_filename
                         , cohort
-                        , display_step):
+                        , display_step
+                        , logging_milestone):
     # ==== Some constant containing field indexes
     start_time = time.time()
     enrolid_idx = 0
@@ -55,15 +59,16 @@ def extract_medications(meds_rawdata_filename
         # pdb.set_trace()
         #==== While not end of the medication file
         line_counter = 0
+        patient_num = 0
         while end_of_med_file == False:
             if line_counter%display_step==0:
                 print('Finished processing {} medication records out of {} records.'.format(line_counter,total_num_records))
             #==== Reading medications visits for current patients
             if line_med[enrolid_idx] == 'NULL':
                 pdb.set_trace()
-            current_enrolid_med = int(line_med[enrolid_idx])
+            current_enrolid_med = float(line_med[enrolid_idx])
             current_patient_meds = []
-            while current_enrolid_med == int(line_med[enrolid_idx]):
+            while current_enrolid_med == float(line_med[enrolid_idx]):
                 current_patient_meds.append(line_med)
                 line_med = meds_raw_file.readline().replace("'",'').rstrip('\n\\n\r\\r')
                 line_counter+=1
@@ -73,11 +78,14 @@ def extract_medications(meds_rawdata_filename
                     end_of_med_file = True
                     break
                 line_med = line_med.split(',')
-                while line_med[enrolid_idx] == 'NULL':
+                while line_med[enrolid_idx] == 'NULL' or line_med[enrolid_idx] == '':
                     line_med = meds_raw_file.readline().replace("'",'').rstrip('\n\\n\r\\r')
                     line_counter+=1
                     if line_counter%display_step==0:
                         print('Finished processing {} medication records out of {} records.'.format(line_counter,total_num_records))
+                    if line_med == '' or line_med == ['']:
+                        end_of_med_file = True
+                        break                    
                     line_med = line_med.split(',')                    
                 if line_med == '' or line_med == ['']:
                     end_of_med_file = True
@@ -120,10 +128,14 @@ def extract_medications(meds_rawdata_filename
             # pdb.set_trace()
             medications_file.write(','.join(map(repr, current_patient_meds_stream)))
             medications_file.write('\n')
-    print('Finished processing all {} of medication records for the {} cohort in {}'.format(total_num_records, cohort, (time.time() - start_time)))
+            patient_num +=1
+            if patient_num%logging_milestone==0:
+                logging.info('Completed extracting and writing the medication stream for {} patient with ENROLID = {}.'.format(cohort,current_enrolid_med))
+    logging.info('Finished processing all {} of medication records for the {} cohort in {}'.format(total_num_records, cohort, (time.time() - start_time)))
+    logging.info('================================')
     return 0        
 
-def extract_diagnoses(diags_rawdata_filename, cohort, display_step):
+def extract_diagnoses(diags_rawdata_filename, cohort, display_step, logging_milestone):
     start_time = time.time()    
     # ==== Some constant containing field indexes
     enrolid_idx = 0
@@ -148,17 +160,19 @@ def extract_diagnoses(diags_rawdata_filename, cohort, display_step):
 
         #==== While not end of the diagnoses file
         line_counter = 0
+        patient_num = 0
         while end_of_diag_file == False:
             if line_counter%display_step==0:
                 print('Finished processing {} diagnoses records out of {} records.'.format(line_counter,total_num_records))
-
             #==== Reading diagnoses visits for current patients
             if line_diag[enrolid_idx] == 'NULL':
                 pdb.set_trace()            
-            current_enrolid_diag = int(line_diag[enrolid_idx])
+            current_enrolid_diag = float(line_diag[enrolid_idx])
+            # if current_enrolid_diag == 33603167001:
+            #     pdb.set_trace()
             current_patient_diags = []
 
-            while current_enrolid_diag == int(line_diag[enrolid_idx]):
+            while current_enrolid_diag == float(line_diag[enrolid_idx]):
                 current_patient_diags.append(line_diag)
                 line_diag = diags_raw_file.readline().replace('\n','').replace("'","").rstrip('\n\\n\r\\r')
                 line_counter+=1
@@ -168,11 +182,15 @@ def extract_diagnoses(diags_rawdata_filename, cohort, display_step):
                     end_of_diag_file = True
                     break
                 line_diag = line_diag.split(',')
-                while line_diag[enrolid_idx] == 'NULL':
+                while line_diag[enrolid_idx] == 'NULL' or line_diag[enrolid_idx] == '':
+                    # pdb.set_trace()
                     line_diag = diags_raw_file.readline().replace('\n','').replace("'","").rstrip('\n\\n\r\\r')
                     line_counter+=1
                     if line_counter%display_step==0:
                         print('Finished processing {} diagnoses records out of {} records.'.format(line_counter,total_num_records))                    
+                    if line_diag == '' or line_diag == ['']:
+                        end_of_diag_file = True
+                        break                    
                     line_diag = line_diag.split(',')  
                 if line_diag == '' or line_diag == ['']:                   
                     end_of_diag_file = True
@@ -214,11 +232,19 @@ def extract_diagnoses(diags_rawdata_filename, cohort, display_step):
                 current_patient_diags_stream.append('EOV')
             diagnoses_file.write(','.join(map(repr, current_patient_diags_stream)))
             diagnoses_file.write('\n')
+            patient_num += 1
+            if patient_num%logging_milestone==0:
+                logging.info('Completed extracting and writing the diagnoses stream for {} patient with ENROLID = {}.'.format(cohort, current_enrolid_diag))
     print('Finished processing all {} of diagnoses records for the {} cohort in {}'.format(total_num_records, cohort, (time.time() - start_time)))            
+    logging.info('Finished processing all {} of diagnoses records for the {} cohort in {}'.format(total_num_records, cohort, (time.time() - start_time)))            
+    logging.info('================================')    
     return 0      
 
 
-def extract_procedures(procs_rawdata_filename, cohort, display_step):
+def extract_procedures(procs_rawdata_filename
+                    , cohort
+                    , display_step
+                    , logging_milestone):
     start_time = time.time()    
     # ==== Some constant containing field indexes
     enrolid_idx = 0
@@ -241,16 +267,16 @@ def extract_procedures(procs_rawdata_filename, cohort, display_step):
 
         #==== While not end of the procedures file
         line_counter = 0
+        patient_num = 0
         while end_of_proc_file == False:
             if line_counter%display_step==0:
                 print('Finished processing {} procedures records out of {} records.'.format(line_counter,total_num_records))
-
             #==== Reading procedures visits for current patients
             if line_proc[enrolid_idx] == 'NULL':
                 pdb.set_trace()            
-            current_enrolid_proc = int(line_proc[enrolid_idx])
+            current_enrolid_proc = float(line_proc[enrolid_idx])
             current_patient_procs = []
-            while current_enrolid_proc == int(line_proc[enrolid_idx]):
+            while current_enrolid_proc == float(line_proc[enrolid_idx]):
                 current_patient_procs.append(line_proc)
                 line_proc = procs_raw_file.readline().replace('\n','').replace("'","").rstrip('\n\\n\r\\r')
                 line_counter+=1
@@ -260,11 +286,14 @@ def extract_procedures(procs_rawdata_filename, cohort, display_step):
                     end_of_proc_file = True
                     break
                 line_proc = line_proc.split(',')
-                while line_proc[enrolid_idx] == 'NULL':
+                while line_proc[enrolid_idx] == 'NULL' or line_proc[enrolid_idx] == '':
                     line_proc = procs_raw_file.readline().replace('\n','').replace("'","").rstrip('\n\\n\r\\r')
                     line_counter+=1
                     if line_counter%display_step==0:
                         print('Finished processing {} procedures records out of {} records.'.format(line_counter,total_num_records))                    
+                    if line_proc == '' or line_proc == ['']:
+                        end_of_proc_file = True
+                        break                    
                     line_proc = line_proc.split(',')  
                 if line_proc == '' or line_proc == ['']:                   
                     end_of_proc_file = True
@@ -304,7 +333,12 @@ def extract_procedures(procs_rawdata_filename, cohort, display_step):
                 current_patient_procs_stream.append('EOV')
             procedures_file.write(','.join(map(repr, current_patient_procs_stream)))
             procedures_file.write('\n')
+            patient_num += 1
+            if patient_num%logging_milestone==0:
+                logging.info('Completed extracting and writing the procedures stream for {} patient with ENROLID = {}.'.format(cohort, current_enrolid_proc))            
     print('Finished processing all {} of procedures records for the {} cohort in {}'.format(total_num_records, cohort, (time.time() - start_time)))            
+    logging.info('Finished processing all {} of procedures records for the {} cohort in {}'.format(total_num_records, cohort, (time.time() - start_time)))            
+    logging.info('================================')    
     return 0  
 
 
