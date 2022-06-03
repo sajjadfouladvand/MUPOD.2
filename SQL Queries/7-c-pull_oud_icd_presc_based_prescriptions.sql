fe36795c507b57c94f154e1fe7992578d58ec23b
Sajjad Fouladvand <34069771+sajjadfouladvand@users.noreply.github.com>
2022-06-03 09:17:26 -0700

    Update 7-c-pull_oud_icd_presc_based_prescriptions.sql

--- SQL Queries/7-c-pull_oud_icd_presc_based_prescriptions.sql
+++ SQL Queries/7-c-pull_oud_icd_presc_based_prescriptions.sql
@@ -1,3 +1,6 @@
+-- This query extracts medication (prescription) records for the OUD cohort. 
+-- Note, since the result table is too large we used views and didn't actually generated the table. Then we use the MS SQL Server IDE to export the view in a csv file.
+
 --DROP TABLE IF EXISTS  [usr_sfouladvand].[dbo].[oud_yes_icd_presc_based_prescriptions];
 USE usr_sfouladvand;
 GO
