4df713f1a85dae557b5a92358b0e29ccdb9dea65
Sajjad Fouladvand <34069771+sajjadfouladvand@users.noreply.github.com>
2022-06-03 09:19:09 -0700

    Update 7-d-pull_oud_icd_presc_based_procedures.sql

--- SQL Queries/7-d-pull_oud_icd_presc_based_procedures.sql
+++ SQL Queries/7-d-pull_oud_icd_presc_based_procedures.sql
@@ -1,3 +1,9 @@
+-- This query extracts procedure records for the OUD cohort. 
+-- Note, ENCOUNTER_PROCEDURE table contain procedure records. 
+-- Note, since the result table is too large we used views and didn't actually generated the table. Then we use the MS SQL Server IDE to export the view in a csv file.
+
+
+
 --DROP TABLE IF EXISTS  [usr_sfouladvand].[dbo].[oud_yes_icd_presc_based_procedures];
 USE usr_sfouladvand;
 GO
